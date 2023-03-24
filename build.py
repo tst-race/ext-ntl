7b5d83710b739279c3c977bfae47391b66d3a5d3
Kyle Treubig <kyle.treubig@twosixlabs.com>
2023-03-24 14:09:03 -0400

    Format build.py

--- build.py
+++ build.py
@@ -27,7 +27,13 @@ import race_ext_builder as builder
 
 def get_cli_arguments():
     """Parse command-line arguments to the script"""
-    parser = builder.get_arg_parser("ntl", "11.4.4", 1, __file__, [builder.TARGET_LINUX_x86_64, builder.TARGET_LINUX_arm64_v8a])
+    parser = builder.get_arg_parser(
+        "ntl",
+        "11.4.4",
+        1,
+        __file__,
+        [builder.TARGET_LINUX_x86_64, builder.TARGET_LINUX_arm64_v8a],
+    )
     return builder.normalize_args(parser.parse_args())
 
 
@@ -36,9 +42,12 @@ if __name__ == "__main__":
     builder.make_dirs(args)
     builder.setup_logger(args)
 
-    builder.install_packages(args, [
-        ("libgmp3-dev", "2:6.2.*", True),
-    ])
+    builder.install_packages(
+        args,
+        [
+            ("libgmp3-dev", "2:6.2.*", True),
+        ],
+    )
 
     builder.fetch_source(
         args=args,
@@ -51,21 +60,36 @@ if __name__ == "__main__":
 
     logging.root.info("Configuring build")
 
-    builder.execute(args, [
-        "./configure",
-        f"CXX={env['CXX']}",
-    ], cwd=source_dir, env=env)
+    builder.execute(
+        args,
+        [
+            "./configure",
+            f"CXX={env['CXX']}",
+        ],
+        cwd=source_dir,
+        env=env,
+    )
 
     logging.root.info("Building")
-    builder.execute(args, [
-        "make",
-        "-j",
-        args.num_threads,
-    ], cwd=source_dir, env=env)
-    builder.execute(args, [
-        "make",
-        f"DESTDIR={args.install_dir}",
-        "install",
-    ], cwd=source_dir, env=env)
+    builder.execute(
+        args,
+        [
+            "make",
+            "-j",
+            args.num_threads,
+        ],
+        cwd=source_dir,
+        env=env,
+    )
+    builder.execute(
+        args,
+        [
+            "make",
+            f"DESTDIR={args.install_dir}",
+            "install",
+        ],
+        cwd=source_dir,
+        env=env,
+    )
 
     builder.create_package(args)
